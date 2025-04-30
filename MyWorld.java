15b5bfeff0df3fc97a4b6526f9bfbc10dcc9ff6a
xxMichaelxx1 <woushinidie@gmail.com>
2025-04-30 08:24:57 -0400

    Creates the initial position for Elephant

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,15 @@
 import greenfoot.*;
 
+/**
+ * The World my hero lives in.
+ * 
+ * @author Michael Chen
+ * @version April 2025
+ */
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 200);
     }
 }
