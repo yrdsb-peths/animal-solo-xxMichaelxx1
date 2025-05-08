4c81e52057ef7948ae74fe6158ff0b19dfdc6829
xxMichaelxx1 <woushinidie@gmail.com>
2025-05-08 08:23:06 -0400

    Creates an animation timer

--- Elephant.java
+++ Elephant.java
@@ -13,6 +13,7 @@ public class Elephant extends Actor
     GreenfootImage[] idleLeft = new GreenfootImage[8];
     // Direction the elephant is facing
     String facing = "right";
+    SimpleTimer animationTimer = new SimpleTimer();
     
     /**
      * Constructor - The code that gets run one time when object is created
@@ -32,6 +33,8 @@ public class Elephant extends Actor
             idleLeft[i].scale(100, 100);
         }
         
+        animationTimer.mark();
+        
         // Initial elephant image
         setImage(idleRight[0]);
     }
@@ -42,6 +45,12 @@ public class Elephant extends Actor
     int imageIndex = 0;
     public void animateElephant()
     {
+        if(animationTimer.millisElapsed() < 100)
+        {
+            return;
+        }
+        animationTimer.mark();
+
         if(facing.equals("right"))
         {
             setImage(idleRight[imageIndex]);
